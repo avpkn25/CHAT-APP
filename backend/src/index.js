a3883ad8f9da27d652634c96da7fe7b0dcf5ce34
praveenkumar <praveenkumarnaidu88@gmail.com>
2025-06-19 11:57:19 +0530

    completed backend part of messages

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser";
 
 import connectDB from "./lib/db.js";
 import authRoutes from "./routes/authRoutes.js";
+import messageRoutes from "./routes/messageRoutes.js";
 
 dotenv.config();
 const app = express();
@@ -14,6 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on ${port}`);
